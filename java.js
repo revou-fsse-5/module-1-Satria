2f6862c56798c60a69f69fa2f56304dd8a9e16e7
satria <satrialimpad@gmail.com>
2024-06-21 01:27:07 +0700

    penambahn java

---
+++ java.js
@@ -0,0 +1,10 @@
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const menuBtn = document.querySelector('.menu-btn');
+        const navbar = document.querySelector('.navbar');
+
+        menuBtn.addEventListener('click', function() {
+            navbar.classList.toggle('active');
+        });
+    });
+</script>
